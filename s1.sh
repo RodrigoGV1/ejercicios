127c4b3bb32e38f73a4e0ceacfddc38bb1abbfa9
soel <soel@mail.com>
2019-07-24 11:00:40 -0500

    agregué scripts del ejercicio2

---
+++ s1.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+echo "inicia el script 1"
+
+ls .
+
+mkdir Nintendo
+
+touch Nintendo/Mario
+
+echo "let's a go" > Nintendo/Mario
+
+cat Nintendo/Mario
+
+echo "Fin del archivo 1"
---
+++ s2.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+echo "iniciar el script 2"
+
+./s1.sh
+
+touch $1
+
+ls .
+
+mv $1 $2
+
+echo "fin del script 2"
